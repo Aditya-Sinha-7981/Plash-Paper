7a0fa9075791255f26b10ba071d80e4641b0b814
Aaditya Sinha <adityasinha@Lucilla.local>
2026-03-24 15:53:51 +0530

    Changed the internal structure for better optimization and working

--- README.md
+++ README.md
@@ -2,3 +2,11 @@
 This is a simply website repo that will be used for the app Plash, I couldn't find some good slideshow with custom widgets and lower resource usage and came across Plash app, so decided to build my own website to use as wallpaper
 
 Plash local website does not take JS in a different file, as long as JS is inside the HTML, it works properly
+
+
+I have made it in layers so that it will be easy to add stuff in future if needed
+
+First Layer(z-index: 0)
+This is the background image layer, in this, I have added a singular div, made it's position fixed with full screen size
+
+In js, we take the current date, convert it into a hash number(to make sure each date gets a unique value) and then use that value modulo wallpapers.length to always get a correct index based on the day
---
+++ script.js
@@ -0,0 +1,24 @@
+const wallpapers = [
+    "bgs/wallpaper1.jpg",
+    "bgs/wallpaper2.webp"
+];
+const background = document.querySelector("#backgroundDiv");
+
+let dayObject = new Date()
+let currentDay = `${dayObject.getDate()}-${dayObject.getMonth() + 1}-${dayObject.getFullYear()}`
+// Always gives an index value based on current date
+let imageIndex = (generateHash(currentDay) % wallpapers.length)
+
+background.style.backgroundImage = `url(${wallpapers[imageIndex]})`
+
+
+function generateHash(string) {
+    let hash = 0
+    for(const i of string){
+        //This is basically saying leftshift by 5(which means multiply by 32) - hash(to get *31) + charCode
+        hash = (hash << 5) - hash + i.charCodeAt(0)
+        hash |= 0; // Constrain to 32bit integer
+        
+    }
+    return Math.abs(hash) //returning absolute cause it's SIGNED, so it can get negative and JS doesn't accept negative indexes
+}
