84392d022d8a128a8af56b9e8fdee707965b729d
Aditya-Sinha-7981 <aditya2005sinha@gmail.com>
2026-03-28 21:06:13 +0530

    Rain engine and animation has been added and working

--- engine.js
+++ engine.js
@@ -1,15 +1,16 @@
 const canvas = document.getElementById('drawCanvas')
 const ctx = canvas.getContext('2d')
 const rainHeight = 40
-const rainAngle = 45
-const speed = 10
+const rainAngle = 0
+const speed = 25
+const fps = 60
 let raindrops = []
 
 // Need to set canvas width manually or it would just scale, not have actual pixels and thus will look blurry
 ctx.canvas.width = window.innerWidth
 ctx.canvas.height = window.innerHeight
 
-for (let i = 0; i < 10; i++) {
+for (let i = 0; i < 100; i++) {
     raindrops.push(createRaindrop())
     drawRain(raindrops[i])
 }
@@ -17,7 +18,7 @@ for (let i = 0; i < 10; i++) {
 function drawRain(raindrop){
     ctx.beginPath()
     ctx.moveTo(raindrop.x, raindrop.y)
-    ctx.lineTo(raindrop.x + raindrop.dx, rainHeight)
+    ctx.lineTo(raindrop.x + (raindrop.dx * rainHeight), raindrop.y + (raindrop.dy * rainHeight))
     ctx.lineCap = "round"
     ctx.strokeStyle = "white"
     ctx.stroke()
@@ -25,20 +26,43 @@ function drawRain(raindrop){
 
 function createRaindrop(){
     let x = getRandomInt(0, window.innerWidth - rainAngle)
-    let spd = getRandomInt(speed, speed+10)
+    let spd = getRandomInt(speed, speed+5)
     let variation = getRandomInt(-2, 3)
     // dx is dependent of each drops speed * tan of rainangle with some variation, js expected radians so converted them to rads
-    let dx = spd * Math.tan((rainAngle + variation) * (Math.PI / 180))
+    let dx = Math.sin((rainAngle + variation) * (Math.PI / 180))
+    let dy = Math.cos((rainAngle + variation) * (Math.PI / 180))
     // In case I wanna do a same slope rain
     // let dx = speed * Math.tan((rainAngle) * (Math.PI / 180))
     let y = 10
-    return {x, y, spd, dx}
+    return {x, y, dx, dy, spd}
 }
 
-function animateRain(){
-    requestAnimationFrame(animateRain)
+function animateRain(lastCall){
+    if (lastCall > 1000/fps){
+        console.log(lastCall)
+        // lastCall = Date.now()
+    }else{
+
+    }
     ctx.clearRect(0, 0, canvas.width, canvas.height)
-    drawRain()
+    for (let i = 0; i < raindrops.length; i++) {
+        const raindrop = raindrops[i];
+        if(raindrop.x > canvas.width || raindrop.y > canvas.height){
+            raindrop.x = getRandomInt(0, window.innerWidth - rainAngle)
+            raindrop.y = getRandomInt(0, window.innerHeight/2)
+        }else{
+            raindrop.x += raindrop.dx * raindrop.spd
+            raindrop.y += raindrop.dy * raindrop.spd
+        }
+        drawRain(raindrop)
+    }
+
+    console.log(performance.now())
+
+    requestAnimationFrame(animateRain)
+    // setTimeout(() => {
+    //     requestAnimationFrame(animateRain)
+    // }, 1000/fps)
 }
 
 function getRandomInt(min, max) {
@@ -49,3 +73,5 @@ function getRandomInt(min, max) {
 function getRandomArbitrary(min, max) {
     return Math.random() * (max - min) + min;
 }
+
+animateRain()
